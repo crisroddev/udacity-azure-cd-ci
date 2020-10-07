1f4ed6a9f636dafbcb1000c6d7a3e20e42b3e713
crodriguez85 <crodriguez85@gmail.com>
2020-10-07 08:57:05 -0300

    Commands

--- command.sh
+++ command.sh
@@ -1,5 +1,4 @@
-az webapp up -n projectName &&\
-az webapp log tail &&\
+az webapp up -n $webAppName \
+https://$webAppName.scm.azurewebsites.net/api/logs/docker \
 pyhton3 -m venv ~/.venv &&\
 source ~/.venv/bin/activate
-
